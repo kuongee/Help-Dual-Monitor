9abf8dcafd3ea88d07f1af3365e2b149af08f00a
kuongee <kuongee@gmail.com>
2019-05-17 20:42:28 +0900

    Play video if video is playing when display changed

--- video.js
+++ video.js
@@ -1,63 +1,67 @@
-const { desktopCapturer, ipcRenderer } = require('electron')
+const { desktopCapturer, ipcRenderer } = require("electron");
 
 let width = window.innerWidth - 20;
 let height = window.innerHeight - 20;
-let video = document.querySelector('video');
+let video = document.querySelector("video");
 video.width = width;
 video.height = height;
 
-let currentCaptureScreen = 'Screen 1';
+let currentCaptureScreen = "Screen 1";
 
-ipcRenderer.on('videoCommand', (event, arg) => {
-  if (arg.message === 'play') {
+ipcRenderer.on("videoCommand", (event, arg) => {
+  if (arg.message === "play") {
     captureVideo(arg.width, arg.height);
-  }
-  else if (arg.message === 'stop') {
+  } else if (arg.message === "stop") {
     stopVideo();
+  } else if (arg.message === "change") {
+    currentCaptureScreen =
+      currentCaptureScreen === "Screen 1" ? "Screen 2" : "Screen 1";
+    const video = document.querySelector("video");
+    if (video.played) captureVideo(arg.width, arg.height);
   }
-  else if(arg.message === 'change') {
-    currentCaptureScreen = currentCaptureScreen === 'Screen 1' ? 'Screen 2' : 'Screen 1';;
-    captureVideo(arg.width, arg.height);
-  }
-})
+});
 
 function captureVideo(mainWidth, mainHeight) {
-  desktopCapturer.getSources({ types: ['window', 'screen'] }, (error, sources) => {
-    if (error) throw error
-    for (let i = 0; i < sources.length; ++i) {
-      if (sources[i].name === currentCaptureScreen) {
-        navigator.mediaDevices.getUserMedia({
-          audio: false,
-          video: {
-            mandatory: {
-              chromeMediaSource: 'desktop',
-              chromeMediaSourceId: sources[i].id,
-              minWidth: mainWidth,
-              maxWidth: mainWidth,
-              minHeight: mainHeight,
-              maxHeight: mainHeight 
-            }
-          }
-        }).then((stream) => handleStream(stream))
-          .catch((e) => handleError(e))
-        return
+  desktopCapturer.getSources(
+    { types: ["window", "screen"] },
+    (error, sources) => {
+      if (error) throw error;
+      for (let i = 0; i < sources.length; ++i) {
+        if (sources[i].name === currentCaptureScreen) {
+          navigator.mediaDevices
+            .getUserMedia({
+              audio: false,
+              video: {
+                mandatory: {
+                  chromeMediaSource: "desktop",
+                  chromeMediaSourceId: sources[i].id,
+                  minWidth: mainWidth,
+                  maxWidth: mainWidth,
+                  minHeight: mainHeight,
+                  maxHeight: mainHeight
+                }
+              }
+            })
+            .then(stream => handleStream(stream))
+            .catch(e => handleError(e));
+          return;
+        }
       }
     }
-  })
+  );
 
   function handleStream(stream) {
-    const video = document.querySelector('video');
+    const video = document.querySelector("video");
     video.srcObject = stream;
-    video.onloadedmetadata = (e) => video.play();
+    video.onloadedmetadata = e => video.play();
   }
 
   function handleError(e) {
-    console.log(e)
+    console.log(e);
   }
 }
 
 function stopVideo() {
-  const video = document.querySelector('video');
-  if (!video.paused)
-    video.pause();
+  const video = document.querySelector("video");
+  if (!video.paused) video.pause();
 }
